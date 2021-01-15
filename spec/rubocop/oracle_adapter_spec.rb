8815c85ba8ec5fc57cd3ae7b5c94e2c81e4823b7
Koichi ITO <koic.ito@gmail.com>
2021-01-16 05:53:37 +0900

    Run `bundle gem rubocop-oracle_adapter`

---
+++ spec/rubocop/oracle_adapter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubocop::OracleAdapter do
+  it "has a version number" do
+    expect(Rubocop::OracleAdapter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
